c6ee2e6f5561cb46e19a2732243f5f143d09d306
B14CK-C4T <testbothunt@gmail.com>
2025-07-26 19:54:58 +0530

    new save feature added

--- infopy.py
+++ infopy.py
@@ -4,6 +4,13 @@ import yaml
 import json
 
 
+def save_file(path, data):
+    with open(path, 'a') as file:
+        for lines in data:
+            file.writelines(lines)
+            file.write("\n")
+
+
 def load_key(path):
     # load key from config.yaml file
     try:
@@ -22,11 +29,13 @@ def main():
     parser.add_argument("-d", required=True, type=str, help="domain name")
     parser.add_argument("--sub", action="store_true", help="subdomain finder")
     parser.add_argument("--whois", action="store_true", help="whois lookup")
+    parser.add_argument("-o", type=str, help="save output")
     args = parser.parse_args()
 
     domain = args.d  # get domain name
     config = load_key("config.yaml")
     api_key = config.get('apikey')
+    save_file_name = args.o  # get save file name
 
     if args.whois:
         url = f"https://api.securitytrails.com/v1/domain/{
@@ -37,18 +46,34 @@ def main():
         if "message" in json.dumps(data):
             print("[!]", data.get("message"))
         else:
-            print(json.dumps(data, intent=2))
+            whois_data = json.dumps(data, indent=2)
+            print(whois_data)
+            # print(json.dumps(data, intent=2))
+            # whois_data = data.get("<set_keys>") #this part is not tested yet
+            # bcz of the free tire API key
+            if save_file_name:
+                save_file(save_file_name, whois_data)
+            else:
+                pass
 
     elif args.sub:
         url = f"https://api.securitytrails.com/v1/domain/{
             domain}/subdomains?apikey={api_key}"
         headers = {"accept": "application/json"}
         response = requests.get(url, headers=headers)
+        sub_data = []  # create empty list to store data for saving this output
         # filtering response
         data = response.json()
         for x in data.get("subdomains"):
+            sub_data.append(x)
             print(f"{x}")  # it print subdomains only
         print("[-] total subdomains:", data.get("subdomain_count"))
+
+        if save_file_name:  # if -o True then this save -
+            # process run otherwise pass
+            save_file(save_file_name, sub_data)
+        else:
+            pass
 
 
 if __name__ == "__main__":
